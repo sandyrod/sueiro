e2042a39b2ba46e313d137935677546ac6314153
Sandy Rodriguez <sandyrod@gmail.com>
2021-12-21 17:52:32 -0400

    home

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,51 @@
+<div>
+    <div class="cards">
+        <div class="filtros"> 
+            <div class="card" style="width: 18rem;">
+                <img src="/img/filtros.png">
+                <div class="card-body">
+                    <p class="card_title">FILTROS</p>
+                </div>
+            </div>
+        </div>
+        <div class="malla"> 
+            <div class="card" style="width: 18rem;">
+                <img src="/img/mallas.png"">
+                <div class="card-body">
+                    <p class="card_title">MALLAS</p>
+                </div>
+            </div>
+        </div>
+        <div class="zarandas"> 
+            <div class="card" style="width: 18rem;">
+                <img src="/img/zarandas.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card_title">ZARANDAS</p>
+                </div>
+            </div>
+        </div>
+        <div class="tamices"> 
+            <div class="card" style="width: 18rem;">
+                <img src="/img/tamices.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card_title">TAMICES</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cuerpo">
+        <span class="cuerpo_title">SUEIRO E HIJOS</span>
+        <br><br>
+        <span class="cuerpo_sudtitle">Es una empresa dedicada a proporcionar soluciones de
+        filtrado, contando con más de 30 años de experiencia en 
+         el rubro suministrando al mercado con un amplio surtido 
+        de productos.</span>
+
+        <button class="flt__btn">MÁS INFORMACIÓN</button>
+    </div>
+    <div class="flt">
+        <img class="flt_img" src="/img/flt.png">
+    </div>
+</div>
+
