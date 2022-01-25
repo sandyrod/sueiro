c5d474c913a3746da85661e99000eef008918ee1
sandyrod <sandy.rodriguez@desarrollowebtotal.com>
2022-01-25 05:55:49 -0400

    correcciones

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::view('/nosotros', 'nosotros');
 Route::view('/home', 'home');
