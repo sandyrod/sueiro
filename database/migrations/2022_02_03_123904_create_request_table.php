ab7c0ca1f93c8ab9649e908c0f19fc6f0ea4d2b2
sandyrod <sandy.rodriguez@desarrollowebtotal.com>
2022-02-03 08:45:47 -0400

    Tabla para solicitudes de presupuesto

---
+++ database/migrations/2022_02_03_123904_create_request_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budgetrequests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('data')->nullable();
+            $table->string('company')->nullable();
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budgetrequest');
+    }
+}
