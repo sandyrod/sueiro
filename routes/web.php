bf785461ed980c9b7a8dcc73a05cdabccd053ba4
Sandy Rodriguez <sandyrod@gmail.com>
2022-02-23 13:08:00 -0400

    cambio en orden historico

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,6 @@ Route::get('/category',        [App\Http\Controllers\HomeController::class, 'cat
 /* Route::get('/products',        [App\Http\Controllers\HomeController::class, 'products'])->name('products'); */
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::get('/remittances',     [App\Http\Controllers\HomeController::class, 'remittances'])->name('remittances');
-Route::get('/order-history',   [App\Http\Controllers\HomeController::class, 'order-history'])->name('order_history');
+Route::get('/order-history',   [App\Http\Controllers\HomeController::class, 'order_history'])->name('order-history');
 Route::get('/quality',         [App\Http\Controllers\HomeController::class, 'quality'])->name('quality');
 Route::get('/shopping',        [App\Http\Controllers\HomeController::class, 'shopping'])->name('shopping');
