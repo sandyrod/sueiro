dbf2410d9c02924d524d1f31042ad5a81d62261c
Sandy <sandyrod@gmail.com>
2022-08-10 17:40:49 -0400

    modificacion carrito de compra

--- app/Http/Livewire/Shopping.php
+++ app/Http/Livewire/Shopping.php
@@ -4,18 +4,26 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use App\Models\Order;
 use App\Models\Shopping as Shoppings;
 use Illuminate\Support\Facades\Auth;
 use DB;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 
 
 
 class Shopping extends Component
 {
-    public $data ,$search, $count_item, $sud_total; 
-    public function render()
+    public $data ,$search, $count_item, $sud_total,$direccion,$telefono,$retiro_sucursal,$envio_domicilio,$contado,$monto,$transferencia_bnc,$ref,$fecha; 
     
+    public $updateMode  = false;
+    public $imputActive = false;
  
+    
+    public function render()
+
+    
     {
         $id = Auth::user()->id;       
         $this->Title = "Shopping";
@@ -49,6 +57,59 @@ class Shopping extends Component
             $this->emit('notify:toast', ['type'  => 'success', 'name' => 'Registro eliminado...']);
             $this->resetInput();
         }
+    }
+    public function reset_cart()
+    {
+        $id = Auth::user()->id;
+        if ($id) {
+            $record = Shoppings::where('shopping.user_id', '=', $id);
+            $record->delete();
+
+            $this->emit('notify:toast', ['type'  => 'success', 'name' => 'Registro eliminado...']);
+            $this->resetInput();
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function save(Request $request)
+    {
+
+        if ($this->updateMode)
+            return $this->update();
+
+        return $this->store();
+    }
+
+
+
+
+    public function store()
+    {
+        $this->validate([
+     /*        'name'      => 'required',
+            'email'     => 'required',
+            'phone'     => 'required',
+            'company'   => 'required'
+ */
+        ]);
+        Order::create([
+            'direccion'          => $this->direccion,
+            'telefono'         => $this->telefono,
+            'retiro_sucursal'         => $this->retiro_sucursal,
+            'envio_domicilio'     => $this->envio_domicilio,
+            'contado'       => $this->contado,
+            'monto' => $this->monto,
+            'transferencia_bnc'    => $this->transferencia_bnc,
+            'ref' => $this->ref,
+            'fecha' => $this->fecha,
+        ]);
+        $this->emit('notify:toast', ['type'  => 'success', 'name' => 'Registro creado...']);
+        $this->resetInput();
     }
     private function emitUpdates()
     {
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $table = 'orders';
+    protected $fillable = ['direccion', 'telefono', 'retiro_sucursal', 'envio_domicilio','contado','monto','transferencia_bnc','ref','fecha'];
     use HasFactory;
 }
