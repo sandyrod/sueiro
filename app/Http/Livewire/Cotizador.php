ec398fb14a1873c8559a392a9b8861ce84c0241c
sandyrod <sandy.rodriguez@desarrollowebtotal.com>
2022-08-15 13:53:24 -0400

    Cotizador

--- app/Http/Livewire/Cotizador.php
+++ app/Http/Livewire/Cotizador.php
@@ -3,17 +3,46 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Product;
+use Illuminate\Support\Facades\Http;
 
 class Cotizador extends Component
 {
 
-    public $data, $search, $diametrodisco, $malla_metalica, $material, $ancho_malla;
+    public $data, $search, $diametrodisco, $malla_metalica, $material, $ancho_malla, $vlr_mtr_cuadrado;
     
     public $updateMode  = false;
     public $imputActive = false;
 
+    public function mount()
+    {
+        // $response = Http::withHeaders([
+        //     'accesstoken' => '03844717-5220-40e7-adba-e1c830091425_13003'
+        // ])->get('https://tiendas.axoft.com/api/Aperture/Product?&pageSize=500&pageNumber=1');
+
+        // $data = $response->json();
+        // //print_r($data['Data']);
+        // foreach($data['Data'] as $producto){
+        //     $product = Product::where('skufield',$producto['SKUCode'])->first();
+        //     if ($product !== null ){
+        //         $product->update(['name' => $producto['Description'],
+        //                 'description' => $producto['AdditionalDescription']]);
+        //     }else{
+        //         $sku = $producto['SKUCode'];
+        //         echo $sku."<br>";
+        //         $product = Product::create(
+        //                 [
+        //                 'name' => $producto['Description'],
+        //                 'description' => $producto['AdditionalDescription'],
+        //                 'skufield' => $sku
+        //             ]);
+        //     }
+        // }
+    }
+
     public function render()
     {
+        $this->material = Product::where('skufield','like','01%')->get();
         return view('livewire.cotizador');
     }
     public function resetInput()
