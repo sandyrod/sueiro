965301d0f9bbdd5ccffaf8b6378e57e9e8382ee1
Sandy <sandyrod@gmail.com>
2022-08-15 09:38:12 -0400

    cambios

--- app/Http/Livewire/Quality.php
+++ app/Http/Livewire/Quality.php
@@ -3,11 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Mail\QualityMailable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
 
 class Quality extends Component
 {
     public function render()
     {
         return view('livewire.quality');
+    }
+
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+        //dd($request);
+        $correo = new QualityMailable($request->all());
+        //dd($correo);
+        Mail::to('sandyrod@gmail.com')->send($correo);
+        // return "mensaje enviado";
+        return view('contact'); 
     }
 }
