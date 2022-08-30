42dac0ae6840e9157c9cadf42aed082e2d070316
sandyrod <sandy.rodriguez@desarrollowebtotal.com>
2022-08-30 18:07:42 -0400

    inicial mayuscula

--- app/Models/settings.php
+++ app/Models/settings.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class settings extends Model
+class Settings extends Model
 {
     protected $table = 'setting';
     protected $fillable = ['dolar', 'euro','costo_referencia','activacion'];
